787a6dd624809f694b1d335b35e3859c68653935
gptlv <usernamegg@mail.ru>
2024-01-12 20:45:42 +0300

    update: 219 solution

--- 219/main.go
+++ 219/main.go
@@ -1,25 +1,23 @@
 package main
 
+import "math"
+
 func main() {
 
 }
 
 func containsNearbyDuplicate(nums []int, k int) bool {
-	idx := 0
 
-	for k != 0 {
-		if idx+k >= len(nums) {
-			k--
-			idx = 0
-			continue
-		}
+	m := make(map[int]int)
 
-		if nums[idx] == nums[idx+k] {
+	for i, num := range nums {
+		if _, ok := m[num]; ok && math.Abs(float64(m[num]-i)) <= float64(k) {
 			return true
 		}
 
-		idx++
+		m[num] = i
 	}
 
 	return false
+
 }
