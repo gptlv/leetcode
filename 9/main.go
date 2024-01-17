c1d81c3f69b4ce84422081e614361ba6046fdf0b
gptlv <usernamegg@mail.ru>
2024-01-17 19:57:19 +0300

    update: 9 solution

--- 9/main.go
+++ 9/main.go
@@ -2,58 +2,24 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
 func main() {
-	fmt.Println(isPalindrome(10000021))
+	fmt.Println(isPalindrome(121))
 }
 
 func isPalindrome(x int) bool {
-
-	// fmt.Println(x)
-
 	if x < 0 {
 		return false
 	}
 
-	len := getNumberLength(x)
+	n := x
+	r := 0
 
-	if len == 1 {
-		return true
+	for n > 0 {
+		r = r*10 + n%10
+		n /= 10
 	}
 
-	if len == 2 {
-		return x/10 == x%10
-	}
-
-	firstDigit := x / powInt(10, len-1)
-	lastDigit := x % 10
-
-	// fmt.Println(firstDigit, lastDigit)
-
-	if firstDigit == lastDigit {
-		digitsInTheMiddle := (x - (firstDigit * powInt(10, len-1))) / 10
-		fmt.Println(digitsInTheMiddle, x)
-		return isPalindrome(digitsInTheMiddle)
-	}
-
-	return false
-}
-
-func powInt(x, y int) int {
-	return int(math.Pow(float64(x), float64(y)))
-}
-
-func getNumberLength(x int) int {
-	// len := 1
-
-	// for q := x; q > 9; {
-	// 	q = q / 10
-	// 	len++
-	// }
-	// return len
-
-	return 1 + int(math.Log10(float64(x)))
-
+	return r == x
 }
