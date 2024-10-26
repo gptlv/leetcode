b1904602056ce7fb0c1f67b6a794accb567ff87d
gptlv <usernamegg@mail.ru>
2024-10-26 10:39:41 +0200

    add: solution for validate-binary-search-tree

---
+++ validate-binary-search-tree/main.go
@@ -0,0 +1,27 @@
+package main
+
+func main() {
+
+}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func isValidBST(root *TreeNode) bool {
+	return helper(root, nil, nil)
+}
+
+func helper(root *TreeNode, min, max *int) bool {
+	if root == nil {
+		return true
+	}
+
+	if min != nil && root.Val <= *min || max != nil && root.Val >= *max {
+		return false
+	}
+
+	return helper(root.Left, min, &root.Val) && helper(root.Right, &root.Val, max)
+}
