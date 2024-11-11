c1d82670ade36c37b11c674cba9e4d0ff40296b3
gptlv <usernamegg@mail.ru>
2024-11-11 10:28:01 +0100

    add: solution for palindrome-number

---
+++ palindrome-number/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "strconv"
+
+func main() {
+
+}
+
+func isPalindrome(x int) bool {
+	str := strconv.Itoa(x)
+	left := 0
+	right := len(str) - 1
+
+	for left <= right {
+		if str[left] != str[right] {
+			return false
+		}
+		left++
+		right--
+	}
+
+	return true
+}
